43d9b690275d1f52693236a83daffc51af24616b
mowahid latif <mowahidlatif@hotmail.com>
2024-08-31 14:45:28 -0500

    Register Feature Added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
 module.exports = {
   registerView: (req, res) => {
     res.render("register");
@@ -7,9 +10,21 @@ module.exports = {
     res.render("login");
   },
 
-  registerUser: (req, res) => {
-    // TODO: complete
-    res.redirect("register");
+  registerUser: async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.render("register", { error: "Please fill all fields" });
+    }
+
+    if (await User.findOne({ where: { email } })) {
+      return res.render("register", {
+        error: "A user account already exists with this email",
+      });
+    }
+
+    await User.create({ name, email, password: bcrypt.hashSync(password, 8) });
+
+    res.redirect("login?registrationdone");
   },
 
   loginUser: (req, res) => {
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const authRoutes = require("./routes/auth");
 const dashboardRoutes = require("./routes/dashboard");
-const contactsRoute = require("./routes/contacts");
-const learnMoreRoute = require("./routes/learnmore");
+const db = require("./db.js");
 
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "pug");
 app.use("/", authRoutes);
 app.use("/", dashboardRoutes);
-app.use("/", contactsRoute);
-app.use("/", learnMoreRoute);
 
-app.listen(PORT, console.log("Server is running on port: " + PORT));
+db.sync({ force: false }).then(() => {
+  app.listen(PORT, console.log("Server is running on port: " + PORT));
+});
